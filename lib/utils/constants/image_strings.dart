1a1abec0a0e734d6d4aa1375e95702c5c99462da
Zaw Min Tun <75002871+zawmorntwan@users.noreply.github.com>
2024-04-11 18:38:42 +0630

    Add size constants and image string constants

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+sealed class AppImageStrings {
+  // App Logos
+  static const String darkAppLogo = 'assets/logos/logo.png';
+  static const String lightAppLogo = 'assets/logos/logo.png';
+
+  // Social Logos
+  static const String google = 'assets/logos/logo.png';
+  static const String facebook = 'assets/logos/logo.png';
+}
