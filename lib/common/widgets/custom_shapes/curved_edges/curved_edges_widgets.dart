0f2d59cfb595832933b10c73c014716f668d37e8
Zaw Min Tun <75002871+zawmorntwan@users.noreply.github.com>
2024-09-04 16:27:42 +0630

    Add custom shape for home screen

---
+++ lib/common/widgets/custom_shapes/curved_edges/curved_edges_widgets.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'curved_edges.dart';
+
+class CurvedEdgesWidget extends StatelessWidget {
+  const CurvedEdgesWidget({
+    super.key,
+    this.child,
+  });
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: AppCustomCurvedEdges(),
+      child: child,
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeScreen'),
-      ),
-      body: Center(
-        child: Text('HomeScreen'),
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            PrimaryHeaderContainer(
+              child: Column(
+                children: [],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
